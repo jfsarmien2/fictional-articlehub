f9f5bf30eaf42d76042a89494ce5f6a640840593
John <john.sarmiento452@gmail.com>
2025-01-16 18:47:12 +0800

    aadd search functionality

--- app/views.py
+++ app/views.py
@@ -4,6 +4,8 @@ from app.models import Article
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, ListView, UpdateView, DeleteView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 
@@ -11,9 +13,14 @@ class ArticleListView(LoginRequiredMixin, ListView):
     template_name = "app/home.html"
     model = Article
     context_object_name = "articles"
+    paginate_by = 5
 
     def get_queryset(self):
-        return Article.objects.filter(creator=self.request.user).order_by("-created_at")
+        search = self.request.GET.get("search")
+        queryset = super().get_queryset().filter(creator=self.request.user)
+        if search:
+            queryset = queryset.filter(title__search=search)
+        return queryset.order_by("-created_at")
 
 
 class CreateArticleView(LoginRequiredMixin, CreateView):
@@ -38,11 +45,17 @@ class UpdateArticleView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
         return self.request.user == self.get_object().creator
 
 
-class DeleteArticleView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+class DeleteArticleView(LoginRequiredMixin, UserPassesTestMixin, SuccessMessageMixin, DeleteView):
     template_name = "app/article_delete.html"
     model = Article
     context_object_name = "article"
+    success_message = "Article deleted succesfully"
     success_url = reverse_lazy("home")
+
+    def post(self, request, *args, **kwargs):
+        messages.success(request, message="Article deleted successfully.",
+                         extra_tags="destructive")
+        return super().post(request, *args, **kwargs)
 
     def test_func(self):
         return self.request.user == self.get_object().creator
