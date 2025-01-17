959ae11e719120b6f7927e57855515d6f4ab198f
John <john.sarmiento452@gmail.com>
2025-01-17 15:45:56 +0800

    updated shell command

--- start-django.sh
+++ start-django.sh
@@ -4,8 +4,8 @@ poetry run python manage.py collectstatic --no-input
 
 poetry run python manage.py migrate
 
-if [[$ENV_STATE == "production"]]; then
-    poetry run gunicorn django_course.wsgi --workers $GUNICORN_WORKERS --forwared-allow-ips "*"
+if [[ $ENV_STATE == "production" ]]; then
+    poetry run gunicorn django_course.wsgi --workers $GUNICORN_WORKERS --forwarded-allow-ips "*"
 else
     poetry run python manage.py runserver 0.0.0.0:8000
 fi
